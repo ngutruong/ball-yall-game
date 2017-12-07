258777f58d28e1700c89898e60dd089fac3a8d20
Vitaliy Shahanyants <vi_taliy@hotmail.com>
2017-12-07 13:47:57 -0500

    Camera Move

---
+++ Assets/Script/ControllCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllCamera : MonoBehaviour {
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        transform.position = player.transform.position + offset;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            transform.LookAt(player.transform);
+            transform.RotateAround(player.transform.position, Vector3.up, Input.GetAxis("Mouse X") * 10f);
+
+        }
+    }
+}
--- Assets/Script/FollowPLayer.cs
+++ Assets/Script/FollowPLayer.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class FollowPLayer : MonoBehaviour {
     [SerializeField]
     private Transform player;
-    [SerializeField]
-    private Vector3 offset;
     // Use this for initialization
     void Start () {
-        offset = transform.position - player.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = player.transform.position + offset;
-        transform.LookAt(player.position);
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.RotateAround(Vector3.zero, Vector3.up, 10f * Time.deltaTime);
-            Debug.Log("D buton was presed");
-        }
-            
+        if (player != null)
+            transform.position = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(player != null)
+            transform.position = player.transform.position;
+        
+        
     }
 }
