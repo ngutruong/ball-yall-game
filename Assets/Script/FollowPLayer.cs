f86f90844b84079abb547726d78863475a966f07
Vitaliy Shahanyants <vi_taliy@hotmail.com>
2017-12-07 12:10:26 -0500

    Initial Commit

---
+++ Assets/Script/FollowPLayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPLayer : MonoBehaviour {
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.position);
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, 10f * Time.deltaTime);
+            Debug.Log("D buton was presed");
+        }
+            
+    }
+}
